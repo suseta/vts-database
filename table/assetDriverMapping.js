4cf4c1a4643a577239e46331ab2649da56387c33
Suseta Datta <suseta.datta@gmail.com>
2024-01-09 13:46:36 +0530

    assetDriverMapping table added but not created

---
+++ table/assetDriverMapping.js
@@ -0,0 +1,36 @@
+const { Client } = require('pg')
+
+require('dotenv').config()
+const UbuntuIP = process.env.UbuntuIP
+const password = process.env.Password
+
+async function customerEntityDetails (database) {
+  const connectionString = `postgresql://postgres:${password}@${UbuntuIP}:5432/${database}`
+  let client = new Client({
+    connectionString
+  })
+  await client.connect()
+  try {
+    const query = `
+        CREATE TABLE IF NOT EXISTS asset_driver_mapping (
+          s_entity_id	VARCHAR(10) REFERENCES entity_details(s_entity_id),
+          s_entity_id_and_name VARCHAR(80),
+          s_asset_id VARCHAR(10),
+          s_drv1_name	VARCHAR(50),
+          s_drv2_name	VARCHAR(50)
+          );
+        `
+    await client.query(query)
+    console.log('Asset Driver Mapping Table created successfully')
+  } catch (error) {
+    console.error('Error creating table:', error)
+  } finally {
+    if (client._ending) {
+      console.error('Error: Connection already closed.')
+    } else {
+      await client.end()
+    }
+  }
+}
+
+customerEntityDetails('navxdb')
