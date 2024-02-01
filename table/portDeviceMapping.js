69376960a70d52efad0dc33ac6582c46c7c815c3
suseta <suseta.datta@gmail.com>
2024-02-01 18:29:25 +0530

    portDeviceMapping

---
+++ table/portDeviceMapping.js
@@ -0,0 +1,35 @@
+const { Client } = require('pg')
+
+require('dotenv').config()
+const UbuntuIP = process.env.UbuntuIP
+const password = process.env.Password
+
+async function customerEntityDetails (database) {
+  const connectionString = `postgresql://postgres:${password}@${UbuntuIP}:5432/${database}`
+  let client = new Client({
+    connectionString
+  })
+  await client.connect()
+  try {
+    const query = `
+        CREATE TABLE IF NOT EXISTS port_device_mapping (
+          i_imei_no VARCHAR(20),
+          i_port_no VARCHAR(6),
+          prt_dvc_mp_dt DATE,
+          prt_dvc_unmp_dt DATE
+          );
+        `
+    await client.query(query)
+    console.log('device port mapping Info Table created successfully')
+  } catch (error) {
+    console.error('Error creating table:', error)
+  } finally {
+    if (client._ending) {
+      console.error('Error: Connection already closed.')
+    } else {
+      await client.end()
+    }
+  }
+}
+
+customerEntityDetails('navxdb')
